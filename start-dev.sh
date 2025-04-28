08980c3c071667bca69fef5fb8f0649d375390b3
jay3ss <mentoce@gmail.com>
2025-04-28 16:10:05 -0700

    Docker compose watch for changing files in dev environment

--- start-dev.sh
+++ start-dev.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker compose -f compose.yaml -f docker-compose.dev.yml up --build --remove-orphans
+docker compose -f compose.yaml -f docker-compose.dev.yml up --build --remove-orphans --watch
